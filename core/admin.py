f1af27d3f161d44e4b43ca2f1414d8abda7308d8
AlexanderPonomarev186 <AI.Ponomarev@urfu.me>
2022-12-24 11:10:15 +0500

    Sending data

---
+++ core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from core.models import *
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name", "year", "cost",)
+    list_display_links = ("pk", "name", "year", "cost",)
+    readonly_fields = ('pk',)
